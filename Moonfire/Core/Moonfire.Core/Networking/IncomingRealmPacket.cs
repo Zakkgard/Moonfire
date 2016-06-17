c228f598823e4c78931e1b7f5c4a35ca98319b6d
Jako <Jako8809@gmail.com>
2016-06-17 23:04:43 +0300

    Rename field.

--- Moonfire/Core/Moonfire.Core/Networking/IncomingRealmPacket.cs
+++ Moonfire/Core/Moonfire.Core/Networking/IncomingRealmPacket.cs
@@ -12,12 +12,12 @@ namespace Moonfire.Core.Networking
         public IncomingRealmPacket(byte[] packet, int length) 
             : base(packet, 0, length)
         {
-            this.Length = base.ReadUInt16();
+            this.Lenght = base.ReadUInt16();
             this.PacketId = (WorldOpCode)base.ReadUInt16();
         }
 
         public WorldOpCode PacketId { get; set; }
 
-        public uint Length { get; set; }
+        public uint Lenght { get; set; }
     }
 }
