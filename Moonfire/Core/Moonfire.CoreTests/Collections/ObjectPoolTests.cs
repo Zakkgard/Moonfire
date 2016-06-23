329fe713ce0bd9299ed594e9968e0d89bcee953f
Jako <Jako8809@gmail.com>
2016-06-23 16:51:10 +0300

    Add tests for .

---
+++ Moonfire/Core/Moonfire.CoreTests/Collections/ObjectPoolTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moonfire.Core.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonfire.Core.Collections.Tests
+{
+    [TestClass()]
+    public class ObjectPoolTests
+    {
+        private ObjectPool<String> sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sut = new ObjectPool<String>(() => "");
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyStringIfNothingWasIn()
+        {
+            String result = sut.GetObject();
+
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyStringWhenAskedTwice()
+        {
+            String result = sut.GetObject();
+
+            Assert.AreEqual("", result);
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnObjectThatWasAlreadyPutInThePull()
+        {
+            sut.PutObject("1");
+
+            Assert.AreEqual("1", sut.GetObject());
+        }
+
+        [TestMethod]
+        public void ShouldReturnObjectsInReveresedOrderOfTheOneTheyWarePutIn()
+        {
+            sut.PutObject("1");
+            sut.PutObject("2");
+
+            Assert.AreEqual("2", sut.GetObject());
+            Assert.AreEqual("1", sut.GetObject());
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyStringIfAllOjbectedThatInThePullArePulledOut()
+        {
+            sut.PutObject("1");
+            sut.PutObject("2");
+
+            sut.GetObject();
+            sut.GetObject();
+
+            Assert.AreEqual("", sut.GetObject());
+        }
+    }
+}
